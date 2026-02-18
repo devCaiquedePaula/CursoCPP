fa038724864a20e74b219445f50008ca591a6812
DevCaique <dev.caiquedepaula@gmail.com>
2026-02-18 15:32:22 -0300

    Finalização da aula sobre variaveis globais, locais e operadores matemáticos

--- aula06/variaveis_globais_e_locais_oper_mat.cpp
+++ aula06/variaveis_globais_e_locais_oper_mat.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "variaveis_globais_e_locais_oper_mat.h"
+#include <iostream>
+
+using namespace std;
+
+int n1, n2; // Variaveis globais, podem ser acessadas por qualquer funcao do programa
+
+int main() {
+
+    string nome; // Variavel local
+
+    cout << "Digite seu nome: ";
+    cin >> nome;
+
+    cout << "Digite o primeiro numero: ";
+    cin >> n1;
+    cout << "Digite o segundo numero: ";
+    cin >> n2;
+
+    cout << "Ola, " << nome << "!" << "\n";
+    cout << "Adicao: " << n1 + n2 << "\n";
+    cout << "Subtracao: " << n1 - n2 << "\n";
+    cout << "Multiplicacao: " << n1 * n2 << "\n";
+    cout << "Divisao: " << n1 / n2 << "\n";
+    cout << "Resto: " << n1 % n2 << "\n";
+
+    return 0;
+}
